f5b04c6e1c5827ea52a534b8ba4d65fbbd109655
Md. Shamim Rahman <apubdtea@gmail.com>
2024-02-27 11:22:25 +0600

    docs: add docstring for the AddressBook class

--- address_book.py
+++ address_book.py
@@ -3,12 +3,18 @@ from pathlib import Path
 
 
 class AddressBook:
+    """A simple attempt to represent a address book."""
+
     def __init__(self, filename):
         self.filename = filename
         self.file_path = Path(filename)
         self.contacts = self.load_contacts()
 
     def load_contacts(self):
+        """
+        Load contacts from the JSON file.
+        If the file doesn't exist or cannot be read, return an empty list.
+        """
         if self.file_path.exists():
             contents = self.file_path.read_text()
             return json.loads(contents)
