076e82de59e28ad51ca0af385066419249337ab4
Md. Shamim Rahman <apubdtea@gmail.com>
2024-03-07 21:52:00 +0600

    feat: add delete_contact method

--- address_book.py
+++ address_book.py
@@ -48,3 +48,18 @@ class AddressBook:
                 print(f"Phone: {contact['phone_number']}, Email: {contact['email']}")
                 return
         print(f"No contact found for: '{search_name}'.")
+
+    def delete_contact(self, first_name, last_name):
+        found_contact = False
+        for i, contact in enumerate(self.contacts):
+            if (
+                contact["first_name"].lower() == first_name.lower()
+                and contact["last_name"].lower() == last_name.lower()
+            ):
+                del self.contacts[i]
+                self.save_contacts()
+                found_contact = True
+                print(f"Contact '{first_name} {last_name}' deleted successfully.")
+                break
+        if not found_contact:
+            print(f"No contact found with name '{first_name} {last_name}'")
