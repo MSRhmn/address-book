c7fefcae5261c3499f95a618bbeeeb70317ce2c0
Md. Shamim Rahman <apubdtea@gmail.com>
2024-08-03 18:41:21 +0600

    chore: include address book app in settings

--- ab_project/settings.py
+++ ab_project/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Custom app
+    "address_book",
+    # Default app
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
