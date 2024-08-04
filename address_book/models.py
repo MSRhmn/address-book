79f882353665756536bfac11f76e45b06f98f8a5
Md. Shamim Rahman <apubdtea@gmail.com>
2024-08-04 11:34:03 +0600

    feat: define address book model

--- address_book/models.py
+++ address_book/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    """Define a address book model"""
+
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    phone_number = models.CharField(max_length=15)
+    email = models.EmailField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
