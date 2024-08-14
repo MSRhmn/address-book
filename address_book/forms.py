8c167f0d51d1f78b3688ab345c228c713a0056f5
Md. Shamim Rahman <apubdtea@gmail.com>
2024-08-14 10:31:08 +0600

    fix: meta class definition in contact form class

--- address_book/forms.py
+++ address_book/forms.py
@@ -4,5 +4,6 @@ from .models import Contact
 
 
 class ContactForm(forms.ModelForm):
-    model = Contact
-    fields = ["first_name", "last_name", "phone_number", "email"]
+    class Meta:
+        model = Contact
+        fields = ["first_name", "last_name", "phone_number", "email"]
